0cc80e300837666b696d18ddc95531e5cd56fe5d
Aniket <anidubey7896@gmail.com>
2024-01-10 00:05:25 +0530

    worked on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,44 @@
-// import logo1 from "Images/logo.svg";
+import "./Sidebar.css";
 import logo from "../Images/logo.svg";
 import dropDown from "../Images/dropDown.svg";
 import settingIcon from "../Images/settingIcon.svg";
-import "../components/Sidebar.css";
+import waterIcon from "../Images/waterIcon.svg";
+
 const Sidebar = () => {
   return (
     <div>
-      <div className="sidebox">
+      <div className="sidebox" >
         <div className="logo-box">
           <div className="logo">
             <img src={logo} alt="logo" />
           </div>
         </div>
-
-        <div className="first">
-          <div className="text">Data-in</div>
-          <div className="energy-box">
-            <img src={settingIcon} alt="settingIcon" />
-            <div className="energy">Energy</div>
-            <img src={dropDown} alt="dropdown" />
-
-            <div></div>
+        <div className="boxes1"> 
+          <div className="first">
+            <div className="text">Data-in</div>
+            <div className="energy-box">
+              <div>
+                <img src={settingIcon} alt="settingIcon" />
+              </div>
+              <div className="energy">Energy</div>
+              <div>
+                <img src={dropDown} alt="dropdown" />
+              </div>
+            </div>
           </div>
-          <div></div>
+          <div className="second">
+            <div>
+              <img src={waterIcon} alt="waterIcon" />
+            </div>
+            <div>Water and Effluents</div>
+            <div>
+              <img src={dropDown} alt="dropdown" />
+            </div>
+          </div>
         </div>
-        <div className="second">
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="boxes2">
+
         </div>
-        <div></div>
       </div>
     </div>
   );
